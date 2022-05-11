7575b89556ed53bebfa00d8508e86bed110329f2
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2022-05-11 14:25:30 +0200

    ApplicationService transactional and lazy loading of holiday replacements

--- src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationServiceImpl.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/application/ApplicationServiceImpl.java
@@ -2,6 +2,7 @@ package org.synyx.urlaubsverwaltung.application.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.synyx.urlaubsverwaltung.absence.DateRange;
 import org.synyx.urlaubsverwaltung.application.vacationtype.VacationCategory;
 import org.synyx.urlaubsverwaltung.person.Person;
@@ -25,6 +26,7 @@ import static org.synyx.urlaubsverwaltung.util.DecimalConverter.toFormattedDecim
  * Implementation of interface {@link ApplicationService}.
  */
 @Service
+@Transactional
 class ApplicationServiceImpl implements ApplicationService {
 
     private final ApplicationRepository applicationRepository;
@@ -35,6 +37,7 @@ class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Application> getApplicationById(Integer id) {
         return applicationRepository.findById(id);
     }
@@ -45,11 +48,13 @@ class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsForACertainPeriodAndPerson(LocalDate startDate, LocalDate endDate, Person person) {
         return applicationRepository.getApplicationsForACertainTimeAndPerson(startDate, endDate, person);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsStartingInACertainPeriodAndPersonAndVacationCategory(LocalDate startDate, LocalDate endDate, Person person, List<ApplicationStatus> statuses, VacationCategory vacationCategory) {
         return applicationRepository.findByStatusInAndPersonAndStartDateBetweenAndVacationTypeCategory(statuses, person, startDate, endDate, vacationCategory);
     }
@@ -60,51 +65,61 @@ class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsForACertainPeriodAndState(LocalDate startDate, LocalDate endDate, ApplicationStatus status) {
         return applicationRepository.getApplicationsForACertainTimeAndState(startDate, endDate, status);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsWhereApplicantShouldBeNotifiedAboutUpcomingApplication(LocalDate from, LocalDate to, List<ApplicationStatus> statuses) {
         return applicationRepository.findByStatusInAndStartDateBetweenAndUpcomingApplicationsReminderSendIsNull(statuses, from, to);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsWhereHolidayReplacementShouldBeNotified(LocalDate from, LocalDate to, List<ApplicationStatus> statuses) {
         return applicationRepository.findByStatusInAndStartDateBetweenAndHolidayReplacementsIsNotEmptyAndUpcomingHolidayReplacementNotificationSendIsNull(statuses, from, to);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getApplicationsForACertainPeriodAndPersonAndState(LocalDate startDate, LocalDate endDate, Person person, ApplicationStatus status) {
         return applicationRepository.getApplicationsForACertainTimeAndPersonAndState(startDate, endDate, person, status);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForStates(List<ApplicationStatus> statuses) {
         return applicationRepository.findByStatusIn(statuses);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForStatesSince(List<ApplicationStatus> statuses, LocalDate since) {
         return applicationRepository.findByStatusInAndEndDateGreaterThanEqual(statuses, since);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForStatesAndPerson(List<ApplicationStatus> statuses, List<Person> persons) {
         return applicationRepository.findByStatusInAndPersonIn(statuses, persons);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForStatesAndPersonSince(List<ApplicationStatus> statuses, List<Person> persons, LocalDate since) {
         return applicationRepository.findByStatusInAndPersonInAndEndDateIsGreaterThanEqual(statuses, persons, since);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForStatesAndPerson(List<ApplicationStatus> statuses, List<Person> persons, LocalDate start, LocalDate end) {
         return applicationRepository.findByStatusInAndPersonInAndEndDateIsGreaterThanEqualAndStartDateIsLessThanEqual(statuses, persons, start, end);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Duration getTotalOvertimeReductionOfPerson(Person person) {
         final BigDecimal overtimeReduction = Optional.ofNullable(applicationRepository.calculateTotalOvertimeReductionOfPerson(person)).orElse(BigDecimal.ZERO);
         return Duration.ofMinutes(overtimeReduction.multiply(BigDecimal.valueOf(60)).longValue());
@@ -129,12 +144,14 @@ class ApplicationServiceImpl implements ApplicationService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Duration getTotalOvertimeReductionOfPersonBefore(Person person, LocalDate date) {
         final BigDecimal overtimeReduction = Optional.ofNullable(applicationRepository.calculateTotalOvertimeReductionOfPersonBefore(person, date)).orElse(BigDecimal.ZERO);
         return Duration.ofMinutes(overtimeReduction.multiply(BigDecimal.valueOf(60)).longValue());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Application> getForHolidayReplacement(Person holidayReplacement, LocalDate date) {
         final List<ApplicationStatus> status = List.of(WAITING, TEMPORARY_ALLOWED, ALLOWED, ALLOWED_CANCELLATION_REQUESTED);
         return applicationRepository.findByHolidayReplacements_PersonAndEndDateIsGreaterThanEqualAndStatusIn(holidayReplacement, date, status);
